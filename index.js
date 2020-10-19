4fdffbcfc6c1ed84f243961a591a1c81ed60eddd
maerd19 <revolutions_333@hotmail.com>
2020-10-19 10:42:16 -0500

    Habilitando CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const conectarDB = require("./config/db");
+const cors = require('cors');
 
 // Crear servidor
 const app = express();
@@ -7,11 +8,19 @@ const app = express();
 // Conectar a la BD's
 conectarDB();
 
+// Habilitar CORS
+console.log(process.env.FRONTEND_URL)
+const opcionesCors = {
+  origin: process.env.FRONTEND_URL
+}
+app.use(cors(opcionesCors));
+
 // Puerto de la app
 const port = process.env.PORT || 4000;
 
 // Habilitar leer los valores de un body
 app.use(express.json());
+
 
 // Rutas de la app
 app.use("/api/usuarios", require("./routes/usuarios"));
